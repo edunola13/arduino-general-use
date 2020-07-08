cc6ed694b623c7c27331d17eea01e2a4e4c5ee7c
Eduardo Nola <edunola13@hotmail.com>
2020-07-08 15:24:59 -0300

    Migrate to PlatformIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
+#include <Arduino.h>
 #include <EEPROM.h>
 
 #include "common_initial.h"
-#define resetPin 2
-#define resetPoint 0
 #include "reset_manage.h"
+#include "serial/StreamCom.h"
 
-#include "api/general_controllers_api.h"
-#include "api/json_interface_api.h"
-#include "ethernet/ethernet_manage.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,4 +13,3 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
-
--- src/reset_manage.h
+++ src/reset_manage.h
@@ -4,6 +4,31 @@
 //  #define resetPin 2
 //  #define resetPoint 0
 
+#ifndef GenUseReset_h
+#define GenUseReset_h
+
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <common_initial.h>
+
+#ifndef resetPin
+  #define resetPin 2
+#endif
+#ifndef resetPoint
+  #define resetPoint 0
+#endif
+
+#ifndef resetPoint
+  #define resetPoint 0
+#endif
+
+#ifndef RESET_BEGIN
+  #define RESET_BEGIN "START RESET"
+#endif
+#ifndef RESET_END
+  #define RESET_END "END RESET"
+#endif
+
 //Llamada por interrupcion
 void resetConfiguration(){
   noInterrupts();
@@ -11,18 +36,20 @@ void resetConfiguration(){
   while(cant < 5){ delay(1); cant++; }
   cant= 0;
   if(digitalRead(resetPin) == LOW){
-    DEB_DO_PRINTLN(reCo);
+    DEB_DO_PRINTLN(RESET_BEGIN);
     EEPROM.update(0, 0);
-    DEB_DO_PRINTLN(enRe);  
-  }  
-  interrupts(); 
+    DEB_DO_PRINTLN(RESET_END);
+  }
+  interrupts();
 }
 
 void initialReset(){
   //Reset PIN
   pinMode(resetPin, INPUT_PULLUP);
   //Aseguro que este abajo
-  digitalWrite(resetPin, HIGH);   
+  digitalWrite(resetPin, HIGH);
   //Inicio interrupciones
   attachInterrupt(resetPoint, resetConfiguration, FALLING);
 }
+
+#endif
